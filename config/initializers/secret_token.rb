213b1950e7265c75506ae4bc41e0bc2b503b4129
DistrictEm <emily@districtem.com>
2013-03-04 17:10:40 -0500

    initial Rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ContractorRatingTool::Application.config.secret_token = '01318b7d7965b6a76e4bb56ea782a0587119d379bfb524e944bdc595ec2b68fe702a6ceeddf0f61c15a74c31622d90563de1d13a768cfdba730dcf18a1da2ddb'
